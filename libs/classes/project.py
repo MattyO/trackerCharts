a5b9ed9243b264c3a7f8fcd26aee396e327e86a9
Matt ODonnell <matt.odonnell@leandog.com>
2012-11-19 23:56:17 -0500

    fixed add states problem with with the use of libral unit testing

--- libs/classes/project.py
+++ libs/classes/project.py
@@ -32,33 +32,67 @@ def burndown_tojson(burndown):
 
 def addState(burndown, stories):
 
-	date = strftime('%Y.%m.%d',localtime())
-	time = strftime('%H:%M:%S',localtime())
-	datetime = date + " " + time 
-
-	state = {"datetime": datetime,"all":{"total":0}}
+	burndown_state = {"all":_initial_state()}
 	for story in stories:
-		if state["all"].has_key(story.current_state) == False:
-			state["all"][story.current_state] = 0
 
 		for label in story.labels:
-			if not state.has_key(label):
-				state[label] = {story.current_state:0}
-			elif label != "datetime" and state[label].has_key(story.current_state) == False:
-				state[label][story.current_state] = 0
-				state[label]["total"] = 0
-			state[label][story.current_state] += 1
-			state[label]["total"] += 1
-		state["all"][story.current_state] += 1
-		state["all"]["total"] += 1
+			if _needs_burndown_label(burndown_state , label):
+				burndown_state = _add_burndown_label(burndown_state, label)
 
-		_normalizeState(state)
+			if _needs_burndown_label_state(burndown_state, label, story.current_state):
+				burndown_state = _add_burndown_label_state(burndown_state, label, story.current_state)
+			if _needs_burndown_label_state(burndown_state, "all", story.current_state):
+				burndown_state = _add_burndown_label_state(burndown_state, "all", story.current_state)
 
-	burndown.states.append(state)
+			burndown_state = _increment_burndown_label_state(burndown_state , label, story.current_state)
+			burndown_state = _increment_burndown_label_state(burndown_state , label, "total")
+			burndown_state = _increment_burndown_label_state(burndown_state , "all", story.current_state)
+			burndown_state = _increment_burndown_label_state(burndown_state , "all", "total")
+
+	_normalize_burndown_state(burndown_state)
+
+	burndown_state = _append_burndown_state_datetime(burndown_state)
+
+	burndown = append_burndown_state(burndown, burndown_state)
 
 	return burndown
 
-def _normalizeState(state):
+def append_burndown_state(burndown, burndown_state):
+	if burndown.states == []:
+		burndown.states  = burndown_state
+	else:
+		burndown.states.append(burndown_state)
+
+	return burndown
+
+def _initial_state():
+	return {"total":0}
+
+def _increment_burndown_label_state(burndown_state, label, story_state):
+	burndown_state[label][story_state] += 1
+	return burndown_state
+
+def _needs_burndown_label(burndown_state, label):
+	return not burndown_state.has_key(label)
+
+def _add_burndown_label(burndown_state, label):
+	burndown_state[label] = _initial_state()
+	return burndown_state
+
+def _needs_burndown_label_state(burndown_state, label, story_state):
+	return not burndown_state[label].has_key(story_state)
+
+def _add_burndown_label_state(burndown_state, label, story_state):
+	burndown_state[label][story_state] = 0
+	return burndown_state
+
+def _append_burndown_state_datetime(state):
+	date = strftime('%Y.%m.%d',localtime())
+	time = strftime('%H:%M:%S',localtime())
+	state["datetime"] = date + " " + time 
+	return state
+
+def _normalize_burndown_state(state):
 	for story_states in state["all"].keys():
 		for state_label in state.keys():
 				if state_label != "datetime" and state[state_label].has_key(story_states) == False:
--- tests/unit/project_test.py
+++ tests/unit/project_test.py
@@ -4,7 +4,8 @@ import xml.etree.ElementTree as ET
 import sys
 sys.path.append("../../libs")
 
-from classes.project import Project, ProjectList, Burndown, addState
+from classes.project import Project, ProjectList, Burndown, addState, _append_burndown_state_datetime, _initial_state, _needs_burndown_label, _needs_burndown_label_state, _add_burndown_label, _add_burndown_label_state, _increment_burndown_label_state
+
 from classes.story import Story
 
 class ProjectTest(unittest.TestCase):
@@ -26,18 +27,52 @@ class ProjectListTest(unittest.TestCase):
 
 class BurndownTest(unittest.TestCase):
 
-    def test_create_class(self):
-        burndown = Burndown("test burndown")
-        self.assertEqual(burndown.states, [])
-        self.assertEqual(burndown.project_name, "test burndown")
+	def test_create_class(self):
+		burndown = Burndown("test burndown")
+		self.assertEqual(burndown.states, [])
+		self.assertEqual(burndown.project_name, "test burndown")
 
+	def test_needs_burndown_label(self):
+		state = {"all":_initial_state()}
+		self.assertTrue(_needs_burndown_label(state, "epic_name"))
+		state["epic_name"] = {"all":_initial_state()}
+		self.assertFalse(_needs_burndown_label(state, "epic_name"))
 
-    def test_add_state(self):
-        burndown = Burndown("test burndown")
-        story_xml = ET.parse("data/story_1").getroot()
-        story = Story(story_xml)
-        burndown = addState(burndown, [story])
-        self.assertEqual(len(burndown.states), 1)
+	def test_needs_burndown_label_state(self):
+		state = {"all":_initial_state()}
+		self.assertTrue(_needs_burndown_label_state(state, "all", "unscheduled"))
+		state["all"]["unscheduled"] = 1
+		self.assertFalse(_needs_burndown_label_state(state, "all", "unscheduled"))
+
+	def test_add_burndown_label(self):
+		state = {"all":_initial_state()}
+		self.assertFalse(state.has_key("epic_name"))
+		state = _add_burndown_label(state, "epic_name")
+		self.assertTrue(state.has_key("epic_name"))
+	
+	def test_add_burndown_label_state(self):
+		state = {"all":_initial_state()}
+		self.assertFalse(state["all"].has_key("unscheduled"))
+		state = _add_burndown_label_state(state,"all", "unscheduled")
+		self.assertTrue(state["all"].has_key("unscheduled"))
+	
+	def test_increment_burndown_state(self):
+		state = {"all":_initial_state(), "epic_name":{"total":0, "unscheduled":0}}
+		_increment_burndown_label_state(state, "epic_name", "unscheduled")
+		self.assertEqual(state, {"all":_initial_state(), "epic_name":{"total":0, "unscheduled":1}})
+
+	def test_append_date_time(self):
+		state = {"all":_initial_state()} 
+		state = _append_burndown_state_datetime(state)
+		self.assertIn("datetime", state)
+
+	def test_add_state(self):
+		burndown = Burndown("test burndown")
+		story_xml = ET.parse("data/story_1").getroot()
+		story = Story(story_xml)
+		burndown = addState(burndown, [story])
+		print burndown.states
+		self.assertEqual(len(burndown.states), 3)
 
 if __name__ == "__main__":
 	unittest.main()
