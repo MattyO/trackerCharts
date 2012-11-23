f8a48571b5c3c40f2bc82e80e6e1ef5b4019d4ee
Matt ODonnell <matt.odonnell@leandog.com>
2012-11-23 02:25:54 -0500

    area chart achived\!\!\!

--- static/js/burndown.js
+++ static/js/burndown.js
@@ -1,43 +1,76 @@
 $(document).ready(function(){
-    $.ajax({
+    /*$.ajax({
         url:"/11111/burndown.json", 
         dataType:"json",
         success: function(data){
-            var lines = new Array(); 
-            $.each(data.states, function(i, state){
+*/
 
-                //alert(labels.datetime);
+        var stateLabels = Array("","","" );
+        var margin = {top: 20, right: 20, bottom: 30, left: 50};
+        var width = 960 - margin.left - margin.right;
+        var height = 500 - margin.top - margin.bottom;
 
-                $.each(state.all, function(key,d){
-                    if(lines[key] == undefined){
-                        lines[key] = new Array();
-                    }
-                    var testobj = {count:d, datetime:state.datetime}
-                    console.log("test object");
-                    console.log(testobj);
-                    lines[key].push({count:d, datetime:state.datetime});
-                });
+        var parseDate = d3.time.format("%Y.%m.%d %H:%M:%S").parse
+
+        var x = d3.time.scale()
+            .range([0,width]);
+        var y = d3.scale.linear()
+            .range([height, 0]);
+
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("bottom");
+
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient("left")
+
+        var area = d3.svg.area()
+            .x(function(d){return x(d.datetime);})
+            .y0(function(d){return height;})
+            .y1(function(d){return y(d.all.total);})
+
+        var svg = d3.select("body")
+            .append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top +")");
+            
+        d3.json("/687393/burndown.json", function(data, error){
+            data = data.states
+
+            $.each(data, function(i,state){
+                data[i].all.total = +state.all.total ;
+                data[i].datetime = parseDate(state.datetime);
+                console.log("total:" + data[i].all.total+","+data[i].datetime);
             });
 
-            console.log("done with main loop.  just checking values");
+            /*data.forEach(function(d){
+                d.all.total = +d.all.total;
+                d.datetime = parseDate(d.datetime);
+            });*/
 
-            console.log(lines);
+            x.domain(d3.extent(data, function(d){ return d.datetime; }));
+            y.domain([0, d3.max(data, function(d){ return d.all.total; })]);
 
-            console.log(lines.started);
-            for (key in lines){
-                for (data_key in lines[key]){
-                    console.log(lines[key][data_key].count + " " + lines[key][data_key].datetime);
-                }
-            }
+            svg.append("path")
+                .datum(data)
+                .attr("class", "area")
+                .attr("d", area);
 
-            $.each(lines, function(i,d){
-                alert(i);
-            });
+            svg.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0,"+height+")")
+                .call(xAxis);
+            svg.append("g")
+                .attr("class", "y axis")
+                .call(yAxis);
 
-                /*
-                $.each(d.all.keys()), function(i,d,key){
-                    lines[key].append(d);
-                });*/
+        });
+
+});
+/*
         }
     });
 
@@ -49,6 +82,36 @@ var burndown = d3.select("#burndown").append("svg");
 
 burndown.selectAll("path")
     .data(
-*/
-    
 
+$.each(data.states, function(i, state){
+
+    //alert(labels.datetime);
+
+    $.each(state.all, function(key,d){
+        if(lines[key] == undefined){
+            lines[key] = new Array();
+        }
+        var testobj = {count:d, datetime:state.datetime}
+        console.log("test object");
+        console.log(testobj);
+        lines[key].push({count:d, datetime:state.datetime});
+    });
+});
+
+console.log("done with main loop.  just checking values");
+
+console.log(lines);
+
+console.log(lines.started);
+for (key in lines){
+    for (data_key in lines[key]){
+        console.log(lines[key][data_key].count + " " + lines[key][data_key].datetime);
+    }
+}
+
+$.each(lines, function(i,d){
+    alert(i);
+});
+
+
+*/
