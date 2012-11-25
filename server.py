298bde28d4c035fb588ff3530e6bbeb184c6083d
Matt ODonnell <matt.odonnell@leandog.com>
2012-11-25 14:24:23 -0500

    daemonized the tornado process

--- server.py
+++ server.py
@@ -2,6 +2,12 @@ from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from app import app
+import daemon
+
+log = open('tornado.log', 'a+')
+ctx = daemon.DaemonContext(stdout=log, stderr=log,
+		working_directory='.')
+ctx.open() 
 
 http_server = HTTPServer(WSGIContainer(app))
 http_server.listen(4567)
