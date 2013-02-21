e86c8c2ec7599605bf6a6ff84d9b70412b667cad
Matt ODonnell <matt.odonnell@leandog.com>
2013-02-20 22:09:06 -0500

    refactoring project tests.  0 deps.  more verbose tests

--- tests/test/project_test.py
+++ tests/test/project_test.py
@@ -13,17 +13,18 @@ from helpers.xml import xml_to_dictonary
 import config
 
 class ProjectTest(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        setattr(cls, 'old_dir', os.getcwd())
-        os.chdir(dirname(__file__))
-
-    @classmethod
-    def tearDownClass(cls):
-        os.chdir(cls.old_dir)
 
     def test_construct_project(self):
-        project_xml = ET.parse("data/project_1").getroot()
+        xml_string = '''
+        <project>
+            <id> 1111111 </id>
+            <name>Test Project</name>
+            <public> false </public>
+            <desciption>There are way more elements than this </desciption>
+        </project>
+
+        '''
+        project_xml = ET.fromstring(xml_string)
         test_project = Project(xml_to_dictonary(project_xml))
 
         self.assertEqual(test_project.name, "Test Project")
@@ -42,17 +43,31 @@ class ProjectTest(unittest.TestCase):
         self.assertEqual(public_projects[1].name, "Project 3")
 
 class ProjectListTest(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        setattr(cls, 'old_dir', os.getcwd())
-        os.chdir(dirname(__file__))
-
-    @classmethod
-    def tearDownClass(cls):
-        os.chdir(cls.old_dir)
 
     def test_construct_project_list(self):
-        project_list_xml = ET.parse("data/projects").getroot()
+        xml_projects = '''
+        <projects> 
+            <project>
+                <id> 1111111 </id>
+                <name>Test Project</name>
+                <public> false </public>
+                <desciption>There are way more elements than this </desciption>
+            </project>
+            <project>
+                <id> 2222222 </id>
+                <name>Test Project 2</name>
+                <public> false </public>
+                <desciption>There are way more elements than this </desciption>
+            </project>
+            <project>
+                <id> 3333333 </id>
+                <name>Test Project again</name>
+                <public> false </public>
+                <desciption>There are way more elements than this </desciption>
+            </project>
+        </projects>
+        '''
+        project_list_xml = ET.fromstring(xml_projects)
         project_list = ProjectList(project_list_xml)
 
         self.assertEqual(len(project_list), 3)
@@ -60,36 +75,52 @@ class ProjectListTest(unittest.TestCase):
         self.assertEqual(project_list[1].name, "Test Project 2")
 
     def test_find_project(self):
-        project_list_xml = ET.parse("data/projects").getroot()
-        project_list = ProjectList(project_list_xml)
+        project_list = [
+                Project({"id": "111111", "public": "true", "name":"Test Project 1"}),
+                Project({"id": "222222", "public": "false", "name":"Test Project 2"}),
+                Project({"id": "666666", "public": "false", "name":"Test Project 3"})]
 
         project_found = find_project(project_list, '666666')
+
         self.assertEqual(project_found.id, '666666')
+        self.assertEqual(project_found.name, 'Test Project 3')
 
     def test_cant_find_project(self):
-        project_list_xml = ET.parse("data/projects").getroot()
-        project_list = ProjectList(project_list_xml)
+        project_list = [
+                Project({"id": "111111", "public": "true", "name":"Test Project 1"}),
+                Project({"id": "222222", "public": "false", "name":"Test Project 2"}),
+                Project({"id": "666666", "public": "false", "name":"Test Project 3"})]
+
         project_found = find_project(project_list,'999999')
 
         self.assertEqual(project_found, None)
 
     def test_filter_on_ids(self):
-        project_list_xml = ET.parse("data/projects").getroot()
-        project_list = ProjectList(project_list_xml)
+        project_list = [
+                Project({"id": "111111", "public": "true", "name":"Test Project 1"}),
+                Project({"id": "222222", "public": "false", "name":"Test Project 2"}),
+                Project({"id": "666666", "public": "false", "name":"Test Project 3"})]
+
         project_list = filter_on_ids(project_list, [666666])
 
         self.assertEqual(len(project_list), 2)
+        self.assertEqual(project_list[0].id, '111111')
 
     def test_construct_private_id_list(self):
-        project_list_xml = ET.parse("data/projects").getroot()
-        project_list = ProjectList(project_list_xml)
+        project_list = [
+                Project({"id": "111111", "public": "true", "name":"Test Project 1"}),
+                Project({"id": "222222", "public": "false", "name":"Test Project 2"}),
+                Project({"id": "333333", "public": "false", "name":"Test Project 3"})]
+
         project_ids = list_private_ids(project_list)
 
         self.assertEquals(project_ids,[222222, 333333])
 
     def test_filter_on_viability(self):
-        project_list_xml = ET.parse("data/projects").getroot()
-        project_list = ProjectList(project_list_xml)
+        project_list = [
+                Project({"id": "111111", "public": "true", "name":"Test Project 1"}),
+                Project({"id": "222222", "public": "false", "name":"Test Project 2"}),
+                Project({"id": "333333", "public": "false", "name":"Test Project 3"})]
 
         private_ids = list_private_ids(project_list)
         project_list = filter_on_ids(project_list, private_ids)
